6a544565638273dc99fa12e1b1b07648b3008ccd
r1chwo0n <woonzy09@gmail.com>
2024-04-04 15:24:34 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Card from "./component/Card";
 
 function App() {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
         <div className="max-w-full max-h-full">
-          {/* {webSocketState.isConnected ? <ChatBox /> : <EnterUsername />} */}
+          <Card />
         </div>
       </div>
     </>
---
+++ frontend/src/component/Card.tsx
@@ -0,0 +1,10 @@
+import "../style/card.css";
+export default function Card() {
+  return (
+    <>
+      <div className="RectangleBlue">
+        <div className="EllipseBlue ml-2"></div>
+      </div>
+    </>
+  );
+}
