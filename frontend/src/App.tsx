b758895218927a4afda05f24f99299069e81a719
r1chwo0n <woonzy09@gmail.com>
2024-04-04 20:40:13 +0700

    add card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,7 @@ import Card from "./component/Card";
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="max-w-full max-h-full">
-          <Card />
-        </div>
-      </div>
+      <Card></Card>
     </>
   );
 }
